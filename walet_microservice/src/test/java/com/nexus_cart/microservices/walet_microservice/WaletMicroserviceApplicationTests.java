0d1b2275bbee145338bc15b6cc6aa57b4ee08555
Ali Radwan <aliradwan792@gmail.com>
2026-08-18 18:43:41 +0300

    add initial wallet microservice

---
+++ walet_microservice/src/test/java/com/nexus_cart/microservices/walet_microservice/WaletMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexus_cart.microservices.walet_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaletMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
