20a7ca735417f3b6072bcb4a68819043764f39af
Ali Radwan <aliradwan792@gmail.com>
2026-08-20 22:27:02 +0300

    add initial setup for inventory microservice

---
+++ inventory_microservice/src/main/java/com/nexus_cart/microservices/inventory_microservice/InventoryMicroserviceApplication.java
@@ -0,0 +1,15 @@
+package com.nexus_cart.microservices.inventory_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class InventoryMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryMicroserviceApplication.class, args);
+	}
+
+}
---
+++ inventory_microservice/src/test/java/com/nexus_cart/microservices/inventory_microservice/InventoryMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexus_cart.microservices.inventory_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
