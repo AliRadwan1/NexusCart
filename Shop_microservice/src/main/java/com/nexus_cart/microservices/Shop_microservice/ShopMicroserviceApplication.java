17bf1a81c4543ab84858483d12f0a103164cf052
Ali Radwan <aliradwan792@gmail.com>
2026-08-21 19:03:03 +0300

    add initial setup/configuration of the shop microservice

---
+++ Shop_microservice/src/main/java/com/nexus_cart/microservices/Shop_microservice/ShopMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.nexus_cart.microservices.Shop_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopMicroserviceApplication.class, args);
+	}
+
+}
---
+++ Shop_microservice/src/test/java/com/nexus_cart/microservices/Shop_microservice/ShopMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexus_cart.microservices.Shop_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
